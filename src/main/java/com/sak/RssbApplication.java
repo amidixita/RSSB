166e48f22a39b3f35ea3afe26dc1aad3f564c1fc
sappy-akb <sapnam701@gmail.com>
2020-02-25 14:29:10 +0530

    First commit

---
+++ src/main/java/com/sak/RssbApplication.java
@@ -0,0 +1,13 @@
+package com.sak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssbApplication.class, args);
+	}
+
+}
